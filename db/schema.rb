d8134048cb164615d670d55396d26c55d48976d7
João Pedro <devjoaomonteiro@gmail.com>
2025-02-09 13:45:45 -0300

    Docker

--- db/schema.rb
+++ db/schema.rb
@@ -46,7 +46,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_06_184749) do
     t.string "name", null: false
     t.string "email", null: false
     t.string "password_digest", null: false
-    t.integer "role"
+    t.integer "role", default: 2, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
